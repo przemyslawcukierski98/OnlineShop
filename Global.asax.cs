c8ea31d67425bdd303e6ed7912dfdd98e9102af5
Przemyslaw Cukierski <przemyslawcukierski98@gmail.com>
2021-01-20 16:42:59 +0100

    Changes have been made to the global.asax file

--- Global.asax.cs
+++ Global.asax.cs
@@ -11,6 +11,24 @@ namespace OnlineShop
     {
         protected void Application_Start(object sender, EventArgs e)
         {
+            Application["StartSerwera"] = DateTime.Now.ToString();
+            Application["LicznikOdwiedzin"] = 0;
+            Application["LiczbaPolaczen"] = 0;
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            Session.Timeout = 1;
+            Session["Zalogowano"] = 0;
+            Session["StartSesji"] = DateTime.Now.ToString();
+            Application["LicznikOdwiedzin"] = (int)Application["LicznikOdwiedzin"] + 1;
+            Application["LiczbaPolaczen"] = (int)Application["LiczbaPolaczen"] + 1;
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+            //zakonczenie sesji. Zmniejszenie licznika połączeń.
+            Application["LiczbaPolaczen"] = (int)Application["LiczbaPolaczen"] - 1;
         }
     }
 }
